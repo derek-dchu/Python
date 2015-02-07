6ee1280c3226533a7d800912cdc179305798a243
Derek Hu <derek.dchu@gmail.com>
2015-02-06 21:24:17 -0500

    add new algorithm using counter

--- algorithms/search/k_th_min.py
+++ algorithms/search/k_th_min.py
@@ -1,4 +1,5 @@
-from ..utils import partition
+from ..utils import partition, random_list_gen
+
 
 class k_th_min:
     """
@@ -14,8 +15,8 @@ class k_th_min:
     # O(n log n), sort the array and return the kth item
     @staticmethod
     def sort_first(iterable, k):
-        if iterable is None or k > len(iterable):
-            raise ValueError("The iterable doesn't contain %d elements" % k)
+        if iterable is None or k <= 0 or k > len(iterable):
+            raise ValueError("Selected element out of bounds")
 
         iterable.sort()
         return iterable[k-1]
@@ -23,24 +24,67 @@ class k_th_min:
     # O(n) quickselect
     @staticmethod
     def quickselect(iterable, k):
-        k -= 1
-        if iterable is None or k > len(iterable):
-            raise ValueError("The iterable doesn't contain %d elements" % k)
+        if iterable is None or k <= 0 or k > len(iterable):
+            raise ValueError("Selected element out of bounds")
 
+        k -= 1
         left = 0
         right = len(iterable)-1
-        while True:
+        while right > left:
             pivot = partition(iterable, left, right)
-            if k == pivot:
-                return iterable[k]
-            elif k < pivot:
+            if pivot < k:
+                left = pivot + 1
+            elif pivot > k:
                 right = pivot - 1
             else:
-                left = pivot + 1
+                return iterable[k]
+        return iterable[left]
+
+    @staticmethod
+    def use_counter(iterable, k):
+        if iterable is None or k <= 0 or k > len(iterable):
+            raise ValueError("Selected element out of bounds")
+        
+        count = 0
+        duplicate = 0
+        next_min = float('Inf')
+        l = len(iterable)
+
+        for i in range(0, l):
+            count = 0
+            duplicate = 0
+            if iterable[i] < next_min:
+                for j in range(0, l):
+                    if iterable[i] > iterable[j]:
+                        count += 1
+                        
+                        if count >= k:
+                            if iterable[i] < next_min:
+                                next_min = iterable[i]
+                            break
+                    elif iterable[i] == iterable[j]:
+                        duplicate += 1
+                if count < k <= count + duplicate:
+                    return iterable[i]
+
 
 if __name__ == '__main__':
-    l = [1, 3, 2, 7, 5, 4]
-    print(k_th_min.sort_first(l, 1)) # 1
-    print(k_th_min.sort_first(l, 4)) # 4
-    print(k_th_min.quickselect(l, 1)) # 1
-    print(k_th_min.quickselect(l, len(l))) # 7
+    for _ in range(0, 100):
+        l = random_list_gen(99)
+        l.append(1000)
+        rst = k_th_min.sort_first(l, 10)
+        assert(k_th_min.quickselect(l, 10) == rst)
+        assert(k_th_min.use_counter(l, 10) == rst)
+
+    from ..utils import Timer
+
+    l = random_list_gen(100000)
+    with Timer(True) as t:
+        k_th_min.sort_first(l, 5)
+
+    with Timer(True) as t:
+        k_th_min.quickselect(l, 5)
+
+    with Timer(True) as t:
+        k_th_min.use_counter(l, 5)
+
